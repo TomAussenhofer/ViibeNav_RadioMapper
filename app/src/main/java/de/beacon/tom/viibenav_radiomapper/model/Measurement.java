6a9192f8aa88549b2df8341144b7a20a7a0be4c2
Pinguinmann <Tom.Aussenhofer@gmx.de>
2015-09-22 21:15:06 +0200

    looper entfernt

--- app/src/main/java/de/beacon/tom/viibenav_radiomapper/model/Measurement.java
+++ app/src/main/java/de/beacon/tom/viibenav_radiomapper/model/Measurement.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -78,8 +77,6 @@ public class Measurement {
                 while (it.hasNext()) {
                     OnyxBeacon tmp = it.next();
                     if(!Util.hasSufficientSendingFreq(tmp.getLastSignalMeasured())){
-                        Looper.prepare();
-                        Toast.makeText(main, "Der Beacon hat zu lange zum senden gebraucht.", Toast.LENGTH_LONG);
 //                        tmp.resetMedianMeasurement();
                     }
 
